c7c9fb7e4a8e2862c975af17a59ce6eb8188edf9
Hacked <michael.kaimbacher@htl-wolfsberg.at>
2023-12-20 12:50:46 +0100

    Tutorial3
    Tutorial3 bis Minute 20

--- CarStocker/Assets/Skripts/Car.cs
+++ CarStocker/Assets/Skripts/Car.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
+    public float acceleration;
+    public float rotationSpeed;
+    public float maxSpeed;
+    public Text txtSpeed;
 
     Rigidbody rigid;
     // Start is called before the first frame update
@@ -15,6 +20,60 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rigid.AddForce(transform.forward*10);
+        AddSpeed();
+        AddRotation();
+        AdjustVelocity();
+        txtSpeed.text = "Speed   " + rigid.velocity.magnitude+"Km/h";
+   
     }
+
+
+
+    void AddSpeed()
+    {
+        if(rigid.velocity.magnitude<maxSpeed)
+        {
+            float inputForward = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.forward * acceleration * inputForward);
+        }
+       
+
+
+    }
+
+
+
+    void AddRotation( )
+    {
+        AddSpeed();
+        float rotation = Input.GetAxis("Horizontal");
+        transform.Rotate(new Vector3(0, rotation* rotationSpeed, 0));
+
+    }
+
+
+     void AdjustVelocity()
+    {
+
+
+        Vector3 velocity = rigid.velocity;
+
+        float direction = Vector3.Dot(transform.forward, velocity.normalized);
+
+        if (direction > 0)
+        {
+            velocity = transform.forward * velocity.magnitude;
+        }
+
+        if (direction < 0)
+        {
+            velocity = -transform.forward * velocity.magnitude;
+
+        }
+        rigid.velocity = velocity;
+
+    }
+
+
 }
+
